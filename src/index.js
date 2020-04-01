b74708a60d12d295eb6f8cca00bc4722455749b7
Desi Siti Maryam <desisitimaryam@gmail.com>
2020-04-01 10:37:14 +0700

    install react-router-dom

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import {Register} from "./components/Register";
-
-ReactDOM.render(
-< Register />,
-  document.getElementById('root')
-);
