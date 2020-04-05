9a1f700fc58ad39c143f1e81576de939b69d498f
Desi Siti Maryam <desisitimaryam@gmail.com>
2020-04-05 10:15:41 +0700

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import {Register} from "./Register";
 import {Login} from './Login';
-import {Profile} from './Profile';
-import {EditProfile} from './EditProfile';
 import {Activity} from './Activity';
-import {Delete} from './DeleteAct';
 import {Create} from './CreateAct';
 import {Edit} from './EditAct';
+import {Delete} from './DeleteAct';
+import {Profile} from './Profile';
+import {EditProfile} from './EditProfile';
 import {Admin} from './Admin';
 import {CreateAdm} from './CreateAdm';
 import {DeleteAdm} from './DeleteAdm';
@@ -18,12 +18,12 @@ function App() {
       <div>
         <Route exact path='/' component={Login} />
         <Route path='/register' component={Register} />
-        <Route path='/profile' component={Profile} />
-        <Route path='/editProfile' component={EditProfile} />
         <Route path='/activity' component={Activity} />
-        <Route path='/deleteActivity' component={Delete} />
         <Route path='/addActivity' component={Create} />
         <Route path='/editActivity' component={Edit} />
+        <Route path='/deleteActivity' component={Delete} />
+        <Route path='/profile' component={Profile} />
+        <Route path='/editProfile' component={EditProfile} />
         <Route path='/admin' component={Admin} />
         <Route path='/addAdmin' component={CreateAdm} />
         <Route path='/deleteAdmin' component={DeleteAdm} />
